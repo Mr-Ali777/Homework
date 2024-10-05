1b3dcae5d9e1ff8d9b781e26bcf6f7c38c6ae5d4
Mr-Ali777 <ali_farhat_fin@mail.ru>
2024-10-05 15:23:46 +0300

    Edit Course and Author

--- Home _work/Education_django_HW/course/models.py
+++ Home _work/Education_django_HW/course/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+
+    def __str__(self):
+        return f"{self.pk} - {self.first_name} {self.last_name}"
+
+
 class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название курса")
     description = models.TextField(blank=True, verbose_name="Описание")
     duration = models.DurationField(verbose_name="Продолжительность")
+    course_author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.pk} - {self.title}"
@@ -20,13 +29,7 @@ class Topic(models.Model):
         return f"{self.pk} - {self.topic_name}"
 
 
-class Author(models.Model):
-    first_name = models.CharField(max_length=100, verbose_name="Имя")
-    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
-    course_author = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
 
-    def __str__(self):
-        return f"{self.pk} - {self.first_name} {self.last_name}"
 
 
 
