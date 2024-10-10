083d8359f11626dc787c1032a424dbaedf25e1dc
nikitapt63 <nikitapt63@gmail.com>
2024-10-10 19:42:07 +0300

    fix imports and links

--- Home _work/Education_django_HW/course/views.py
+++ Home _work/Education_django_HW/course/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
-from django.http.response import HttpResponse
 from .models import Topic
-from forms import Topic_form
+from .forms import Topic_form
 
 
 def topic_list_view(request):
     all_topics = Topic.objects.all()
     context = {"all_topics": all_topics}
-    return render(request, template_name='topic_list_view.html', context)
+    return render(request, 'topic_list_view.html', context)
 
 
 def create_topic_view(request):
@@ -15,10 +14,10 @@ def create_topic_view(request):
         form = Topic_form(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('topic_list_view')
+            return redirect('/topic_list')
     else:
         form = Topic_form()
-    return render(request, template_name='create_topic.html', {'form': form})
+    return render(request, 'create_topic.html', {'form': form})
 
 
 
